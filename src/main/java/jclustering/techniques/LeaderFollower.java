ee71fdc7a83dc72de8a14a2e494e41ba1e5deaca
José María Mateos <jmmateos@mce.hggm.es>
2012-09-28 16:20:21 +0200

    Use only amplitude to compare clusters

--- src/main/java/jclustering/techniques/LeaderFollower.java
+++ src/main/java/jclustering/techniques/LeaderFollower.java
@@ -353,15 +353,16 @@ public class LeaderFollower extends ClusteringTechnique
     }
     
     /*
-     * Use this comparator class to compare Clusters between them.
+     * Use this comparator class to compare Clusters between them. The
+     * indicator used is the peak amplitude.
      */
     private class LeaderFollowerClusterComparator 
         implements Comparator<Cluster> {
 
         @Override
         public int compare(Cluster arg0, Cluster arg1) {
-            double score0 = arg0.getPeakMean() * arg0.size();
-            double score1 = arg1.getPeakMean() * arg1.size();
+            double score0 = arg0.getPeakMean();
+            double score1 = arg1.getPeakMean();
             if (score0 < score1) return -1;
             else if (score0 > score1) return 1;
             else return 0;
