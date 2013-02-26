82f6a274e86b2e378211db1f7cee4c46c0b09e73
José María Mateos <jmmateos@mce.hggm.es>
2013-02-26 12:30:21 +0100

    Added an option to show the full PCA image

--- src/main/java/jclustering/techniques/PCA.java
+++ src/main/java/jclustering/techniques/PCA.java
@@ -1,11 +1,20 @@
 package jclustering.techniques;
 
+import java.awt.GridLayout;
+import java.awt.event.ItemEvent;
+
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.EigenDecomposition;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.stat.correlation.StorelessCovariance;
 
 import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
 import jclustering.Voxel;
 import static jclustering.MathUtils.getMaxIndex;
 
@@ -19,6 +28,9 @@ import static jclustering.MathUtils.getMaxIndex;
 public class PCA extends ClusteringTechnique {
 
     private int dimensions;
+    
+    // Should the PCA image be shown after processing?
+    private boolean showPCA = true;
     
     private double[][] normalized_data;
     
@@ -52,6 +64,55 @@ public class PCA extends ClusteringTechnique {
         RealMatrix result = eigenvectors.multiply(
                             normalized_data_matrix.transpose());
         
+        // If the PCA image is to be shown, create a new image with
+        // result.getRowDimension() frames and the original number of 
+        // x, y, z dimensions
+        if (showPCA) {            
+            // Get number of components
+            int components = result.getRowDimension();
+            
+            // Create dynamic image
+            ImagePlus PCA_image = IJ.createImage("PCA image", "16-bit", 
+                                                 dim[0], dim[1],
+                                                 dim[3] * components);
+            PCA_image.setDimensions(1, dim[3], components);
+            PCA_image.setOpenAsHyperStack(true);
+            
+            // Get stack for easy access
+            ImageStack is = PCA_image.getStack();
+            
+            int column_index = 0;
+            
+            // Assign voxels to values. It is important to iterate the image
+            // in the correct order (first x, then y, then slices), because
+            // that is the way the images are normally processed when using
+            // the ImagePlusHypIterator object.
+            for(int z = 0; z < dim[3]; z++) {
+                for(int y = 0; y < dim[1]; y++) {
+                    for(int x = 0; x < dim[0]; x++) {
+                        
+                        // Test it the TAC is noise. If it is noise, 
+                        // jump to the next one
+                        double [] tac = ip.getTAC(x, y, z + 1);
+                        if (skip_noisy && isNoise(tac)) continue;
+                        
+                        // Not noise: get the next column
+                        double [] comp = result.getColumn(column_index++);
+                        
+                        // Iterate through the component and set the values.
+                        // Each row of the component is in one frame.
+                        for (int t = 0; t < components; t++) {
+                            // Get internal slice number
+                            int sn = PCA_image.getStackIndex(1, z + 1, t + 1);
+                            is.setVoxel(x, y, sn, comp[t]);                            
+                        }                        
+                    }
+                }
+            }
+            
+            PCA_image.show();
+        } // End PCA image show.
+        
         // Please note: this is somehow incorrect. As the clustering model
         // that we are following needs one voxel -> one cluster, this step
         // below assigns each voxel to the principal component with the
@@ -71,6 +132,18 @@ public class PCA extends ClusteringTechnique {
             addTACtoCluster(v, max);            
         }        
         
+    }
+    
+    public JPanel makeConfig() {
+        JPanel jp = new JPanel(new GridLayout(1, 2, 5, 5));
+        
+        jp.add(new JLabel("Show PCA image:"));
+        JCheckBox jcb_showPCA = new JCheckBox();
+        jcb_showPCA.setSelected(showPCA);
+        jcb_showPCA.addItemListener(this);
+        jp.add(jcb_showPCA);
+        
+        return jp;        
     }
     
     /*
@@ -154,6 +227,13 @@ public class PCA extends ClusteringTechnique {
         
         return eigen;
     }
-
+    
+    public void itemStateChanged(ItemEvent arg0) {
+        
+        // Check the checkbox for the showPCA variable
+        JCheckBox jcb = (JCheckBox)arg0.getSource();
+        showPCA = jcb.isSelected();
+        
+    }
     
 }
