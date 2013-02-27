a364dc7ee5634c15e2e95f38a6f80efafda4eb9b
José María Mateos <jmmateos@mce.hggm.es>
2013-02-27 15:48:01 +0100

    Unused import

--- src/main/java/jclustering/techniques/PCA.java
+++ src/main/java/jclustering/techniques/PCA.java
@@ -8,7 +8,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
-import org.apache.commons.math3.linear.EigenDecomposition;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.SingularValueDecomposition;
 import org.apache.commons.math3.stat.correlation.StorelessCovariance;
